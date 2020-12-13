6fb8d92216372fbfe96ae4ed4da97df05c48bc99
shashank-k-y <shahsank.sky.8@gmail.com>
2020-12-13 16:06:35 +0530

    serializing user app and validating email password

--- ecom/settings.py
+++ ecom/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+# AUTH_USER_MODEL = "user.CustomUser"
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
